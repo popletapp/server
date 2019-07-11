5be92b08b62b34cc3d6f521ab9d07c7258fe8d66
Blake <bsward8@gmail.com>
2019-07-11 23:54:28 +1000

    feat: new util to generate 'snowflake'-esque ids

---
+++ util/generateID.js
@@ -0,0 +1,10 @@
+export default () => {
+  // ID's in Poplet are generated similarly to Twitter's "snowflakes"
+  // The formula is [the current time] - [Poplet epoch] + [random integer between 1000 and 10,000]
+
+  const currentTime = Date.now();
+  const POPLET_EPOCH = 1561903200000; // The Poplet epoch is the 1st of July, 2019 (AEST)
+  const randomInteger = Math.floor(Math.random() * (1e4 - 1e3 + 1) + 1e3);
+
+  return ((currentTime - POPLET_EPOCH) + randomInteger).toString();
+}
