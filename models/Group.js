05e58f16f2454356b9221c14c26b7c783ee6e453
Blake <bsward8@gmail.com>
2019-07-11 23:53:16 +1000

    feat: new group model

---
+++ models/Group.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const groupSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    unique: true,
+  },
+  createdAt: {
+    type: Date
+  },
+  name: {
+    type: String,
+  },
+  accessibleBy: {
+    type: Array // of member IDs
+  },
+  items: {
+    type: Array // of note IDs (or other things maybe)
+  },
+  options: {
+    type: Object // other things like color, position, width/height
+  }
+});
+
+const Group = mongoose.model('Group', groupSchema);
+
+export default Group;
