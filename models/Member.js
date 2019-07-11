57c379de10f8c15286f92b865622944e5dcfe713
Blake <bsward8@gmail.com>
2019-07-11 23:54:04 +1000

    feat: new member model

---
+++ models/Member.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const memberSchema = new mongoose.Schema({
+  id: {
+    type: String
+  },
+  board: {
+    type: String // board ID
+  },
+  joinedAt: {
+    type: Date
+  },
+  nickname: {
+    type: String
+  },
+  ranks: {
+    type: Array // of rank IDs
+  }
+});
+
+memberSchema.statics.findByBoard = async (board, id) => await this.findOne({ board, id });
+
+const Member = mongoose.model('Member', memberSchema);
+
+export default Member;
