cf802ea1ddd6617c243ad12a2f6863d58d2a8aef
Blake <bsward8@gmail.com>
2019-07-11 23:58:57 +1000

    feat(Note.js): new update method

--- funcs/Note.js
+++ funcs/Note.js
@@ -1,7 +1,8 @@
 import models from './../models';
+import { generateID } from '../util';
 
 async function create (obj) {
-  const id = Math.floor(Math.random() * 6e14);
+  const id = generateID();
   const note = {
     id,
     title: obj.title || null,
@@ -14,26 +15,38 @@ async function create (obj) {
     assignees: [],
     options: obj.options || {}
   };
-  models.Note.create(note, () => {
-    res.status(200)
-  })
 
   const dbNote = new models.Note(note);
   await dbNote.save();
 
   if (!obj.boardID) {
-    throw 'A board ID needs to be provided in the request body';
+    throw new Error('A board ID needs to be provided in the request body');
   }
   try {
-    models.Board.updateOne({ id: obj.boardID }, {
+    await models.Board.updateOne({ id: obj.boardID }, {
       $push: {
         notes: id
       }
     });
   } catch (e) {
-    throw 'Error whilst trying to apply note to board, make sure the board exists and is valid.';
+    throw new Error('Error whilst trying to apply note to board, make sure the board exists and is valid.');
   }
-  return id;
+  return note;
+}
+
+async function update (obj) {
+  return await models.Note.updateOne({ id: obj.id }, {
+    id: obj.id,
+    title: obj.title || null,
+    content: obj.content || null,
+    createdAt: obj.createdAt,
+    createdBy: obj.createdBy,
+    modifiedAt: Date.now(),
+    modifiedBy: obj.user,
+    labels: obj.labels || [],
+    assignees: obj.assignees || [],
+    options: obj.options || {}
+  });
 }
 
 async function getMultiple (array) {
@@ -46,6 +59,7 @@ async function get (id) {
 
 export default {
   create,
+  update,
   getMultiple,
   get
 }
