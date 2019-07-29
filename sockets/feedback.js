e8aef493d4e8da0071c68af975646b1ed46e182a
Blake <bsward8@gmail.com>
2019-07-30 01:29:56 +1000

    fix: change webhook id for feedback

--- sockets/feedback.js
+++ sockets/feedback.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 import config from './../config';
 
 exports.handle = async ({ client, data }) => {
-  await axios.post(`https://discordapp.com/api/webhooks/605019997663920178/${config.webhookToken}`, data);
+  await axios.post(`https://discordapp.com/api/webhooks/605400080249389065/${config.webhookToken}`, data);
 }
