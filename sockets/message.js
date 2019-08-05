b2e4965e0b5b36fc8ba3854bee030a70af04b423
Blake <bsward8@gmail.com>
2019-08-06 00:33:20 +1000

    feat: stop listening when member leaves board

--- sockets/message.js
+++ sockets/message.js
@@ -7,6 +7,14 @@ const PermissionMap = {
 
 exports.handle = ({ client, data }) => {
   if (data && data.type && data.board) {
+    if (data.type === 'JOIN_BOARD') {
+      client.join(board)
+    }
+
+    if (data.type === 'LEAVE_BOARD') {
+      client.leave(board)
+    }
+
     // Ensure this user is in the board and/or has the correct permissions
     if (Board.authorize(data.board, client.userID, PermissionMap[data.type] || Permissions.USER)) {
       client.to(data.board).emit('message', data)
