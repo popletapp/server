3fe808002ab3a02ea5bbbd1557d7dfd8152dae5f
Blake <bsward8@gmail.com>
2020-02-28 06:52:53 +1100

    feat: permission handling on websocket messages

--- sockets/message.js
+++ sockets/message.js
@@ -2,10 +2,23 @@ import { Board } from './../funcs';
 import { Permissions } from './../util/permissions';
 
 const PermissionMap = {
-  UPDATE_NOTE: Permissions.EDITOR
+  UPDATE_NOTE: 'MANAGE_NOTES',
+  CREATE_NOTE: 'MANAGE_NOTES',
+  DELETE_NOTE: 'MANAGE_NOTES',
+  UPDATE_GROUP: 'MANAGE_NOTES',
+  CREATE_GROUP: 'MANAGE_NOTES',
+  DELETE_GROUP: 'MANAGE_NOTES',
+  DELETE_BOARD: 'ADMINISTRATOR',
+  REQUEST_CHATROOMS: 'VIEW_CHATROOMS',
+  CREATE_NOTE_COMMENT: 'ADD_COMMENTS',
+  CREATE_CHATROOM_COMMENT: 'SEND_CHATROOM_MESSAGES',
+  ADD_RANK: [ 'MODERATOR', 'MANAGE_BOARD' ],
+  UPDATE_RANK: [ 'MODERATOR', 'MANAGE_BOARD' ],
+  DELETE_RANK: [ 'MODERATOR', 'MANAGE_BOARD' ],
+  REMOVE_MEMBER: [ 'KICK_MEMBERS', 'BAN_MEMBERS', 'MODERATOR' ]
 }
 
-exports.handle = ({ client, data }) => {
+exports.handle = async ({ client, data }) => {
   if (data && data.type && data.board) {
     if (data.type === 'JOIN_BOARD') {
       client.join(board)
@@ -16,7 +29,8 @@ exports.handle = ({ client, data }) => {
     }
 
     // Ensure this user is in the board and/or has the correct permissions
-    if (Board.authorize(data.board, client.userID, PermissionMap[data.type] || Permissions.USER)) {
+    const authorized = await Board.authorize(data.board, client.userID, PermissionMap[data.type]);
+    if (authorized) {
       client.to(data.board).emit('message', data)
     }
   }
