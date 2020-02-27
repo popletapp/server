d029a49c22a9514301a1127fbe4ffc71b013e5d6
Blake <bsward8@gmail.com>
2020-02-28 06:53:51 +1100

    feat: improvements to blog route

--- routes/Blog.js
+++ routes/Blog.js
@@ -1,19 +1,25 @@
 import express from 'express';
 const router = express.Router();
-import { BlogPost } from './../funcs';
+import { Blog } from './../funcs';
 import { authorization, permissions } from './../util';
 
-router.get(`/create`, authorization, (req, res, next) => {
+router.post(`/create`, authorization, (req, res, next) => {
   if (permissions.isDeveloper(req.user.id)) {
-    BlogPost.create(req.params.id, req.user.id)
-      .then(notes => notes ? res.status(200).json(notes) : res.status(500))
+    Blog.create(req.body)
+      .then(posts => posts ? res.status(200).json(posts) : res.status(500))
       .catch(err => next(err));
   }
 })
 
+router.get(`/posts`, (req, res, next) => {
+  Blog.getMultiple()
+    .then(posts => posts ? res.status(200).json(posts) : res.status(500))
+    .catch(err => next(err));
+})
+
 router.get(`/post/:id`, (req, res, next) => {
-  BlogPost.get(req.params.id, req.user.id)
-    .then(notes => notes ? res.status(200).json(notes) : res.status(500))
+  Blog.get(req.params.id, req.user.id)
+    .then(posts => posts ? res.status(200).json(posts) : res.status(500))
     .catch(err => next(err));
 })
 
