a7beff72136559f9eac477cc065c34e376228dc4
Nevvulo <bsward8@gmail.com>
2020-06-12 11:46:04 +1000

    feat(User): routes for friend endpoints

--- routes/User.js
+++ routes/User.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import { authorization } from './../util'
-import { User } from './../funcs';
+import { User, Notification } from './../funcs';
 
 router.post(`/register`, async (req, res, next) => {
   await User.create(req.body)
@@ -23,6 +23,63 @@ router.post(`/authenticate`, async (req, res, next) => {
 
 router.get(`/me/boards`, authorization, (req, res, next) => {
   User.listBoards(req.user.id)
+        .then(user => user ? res.json(user) : res.status(403))
+        .catch(err => next(err));
+})
+
+router.get(`/me/notifications`, authorization, async (req, res, next) => {
+  if (!req.query) {
+    req.query = {};
+  }
+  await Notification.get(req.user.id, req.query.type, req.query.limit, req.query.position)
+        .then(user => user ? res.json(user) : res.status(403))
+        .catch(err => next(err));
+})
+
+router.get(`/me/home`, authorization, async (req, res, next) => {
+  await User.getHomeContent(req.user.id)
+        .then(user => user ? res.json(user) : res.status(403))
+        .catch(err => next(err));
+})
+
+// Add friend
+router.post(`/me/friends/:friend`, authorization, async (req, res, next) => {
+  await User.addFriend(req.user.id, req.params.friend)
+        .then(user => user ? res.json(user) : res.status(403))
+        .catch(err => next(err));
+})
+
+// Accept friend
+router.post(`/me/friends/accept/:friend`, authorization, async (req, res, next) => {
+  User.acceptFriendRequest(req.user.id, req.params.friend)
+        .then(user => user ? res.json(user) : res.status(403))
+        .catch(err => next(err));
+})
+
+// Edit friend
+router.patch(`/me/friends/:friend`, authorization, async (req, res, next) => {
+  await User.editFriend(req.user.id, req.params.friend, req.body)
+        .then(user => user ? res.json(user) : res.status(403))
+        .catch(err => next(err));
+})
+
+// Delete friend
+router.delete(`/me/friends/:friend`, authorization, async (req, res, next) => {
+  await User.removeFriend(req.user.id, req.params.friend)
+        .then(user => user ? res.json(user) : res.status(403))
+        .catch(err => next(err));
+})
+
+// Get pending friends
+router.get(`/me/friends/pending`, authorization, async (req, res, next) => {
+  await User.getPendingFriendRequests(req.user.id)
+        .then(user => user ? res.json(user) : res.status(400))
+        .catch(err => next(err));
+})
+
+// Get friends
+router.get(`/me/friends`, authorization, async (req, res, next) => {
+  await User.getFriends(req.user.id)
         .then(user => user ? res.json(user) : res.status(403))
         .catch(err => next(err));
 })
