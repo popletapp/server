af09401aa9a25f9e33dae0858139e4d05b4191e0
Nevvulo <bsward8@gmail.com>
2020-06-12 11:45:26 +1000

    feat(Note): routes for note comments

--- routes/Note.js
+++ routes/Note.js
@@ -1,20 +1,6 @@
 import express from 'express';
 const router = express.Router();
 import { authorization } from './../util'
-import { Note } from './../funcs';
-
-
-
-router.get(`/:id`, authorization, function (req, res, next) {
-  if (req.params.id === 'multiple') {
-    Note.getMultiple(req.body.ids)
-        .then(notes => notes ? res.json(notes) : res.status(404))
-        .catch(err => next(err));
-  } else {
-    Note.get(req.body.id)
-        .then(note => note ? res.json(note) : res.status(404))
-        .catch(err => next(err));
-  }
-})
+import { Note, Board } from './../funcs';
 
 export default router;
