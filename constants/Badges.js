ead6d8deb2d01d77360dfe1d1f31eaebb67b2736
Nevvulo <bsward8@gmail.com>
2020-06-12 11:43:10 +1000

    feat: new constants

---
+++ constants/Badges.js
@@ -0,0 +1,6 @@
+export default {
+  1: 'Beta Tester',
+  2: 'Poplet Premium',
+  4: 'Poplet Translator',
+  8: 'Bug Hunter'
+}
---
+++ constants/HomeContentTypes.js
@@ -0,0 +1,23 @@
+export default {
+  // If no title is present on the note ({note.title} = note #{note.ref})
+
+  // ex. {user} accepted your friend request
+  FRIEND_REQUEST_ACCEPTED: 'FRIEND_REQUEST_ACCEPTED',
+  // ex. You are now friends with {user}
+  FRIENDSHIP_CREATED: 'FRIENDSHIP_CREATED',
+
+  // ex. You were assigned to "{note.title}"
+  ASSIGNED_TO_NOTE: 'ASSIGNED_TO_NOTE',
+  // ex. "{note.title}" is due in {time}
+  DUE_DATE_UPCOMING: 'DUE_DATE_UPCOMING',
+  // ex. {user} mentioned you in "{note.title}"
+  MENTIONED_IN_NOTE: 'MENTIONED_IN_NOTE',
+  // ex. {user} created a new note in {board}
+  NOTE_CREATED: 'NOTE_CREATED',
+  // ex. {user} updated "{note.title}" in {board}
+  NOTE_MODIFIED: 'NOTE_MODIFIED',
+  // ex. {user} created a new public board called {board}
+  BOARD_CREATED_BY_FRIEND: 'BOARD_CREATED_BY_FRIEND',
+  // ex. {user} updated the avatar of {board}
+  BOARD_MODIFIED: 'BOARD_MODIFIED',
+}
