7512b9291c3ca966812b677f512ead1c13cdc58c
Nevvulo <bsward8@gmail.com>
2020-06-12 11:45:13 +1000

    feat(User): new functions for friends

--- funcs/User.js
+++ funcs/User.js
@@ -4,6 +4,8 @@ import bcrypt from 'bcrypt'
 import config from './../config'
 import { generateID } from '../util';
 import HTTPError from '../util/HTTPError';
+import HomeContentTypes from '../constants/HomeContentTypes';
+import { HomeContent } from './';
 
 async function authenticate ({ email, password }) {
   const user = await models.User.findOne({ email });
@@ -35,7 +37,7 @@ async function get (id, returnEverything = false) {
 }
 
 async function getMultiple (array) {
-  return await models.User.find({ id: { $in: array } }, { _id: 0, __v: 0, hash: 0, email: 0, boards: 0 });
+  return await models.User.find({ id: { $in: array } }, { _id: 0, __v: 0, hash: 0, email: 0, boards: 0, compact: 0, theme: 0, lang: 0 });
 }
 
 const DEFAULT_SETTINGS = {
@@ -71,6 +73,7 @@ async function create (obj) {
     avatar: obj.avatar || null,
     badges: 0,
     theme: 0,
+    status: 'offline',
     compact: false,
     lang: 'en',
     bot: false
@@ -100,10 +103,69 @@ async function edit (id, obj) {
     theme: obj.theme,
     compact: obj.compact,
     lang: obj.lang,
+    status: obj.status
   };
   const newUser = Object.assign(original, user);
   await models.User.updateOne({ id }, newUser);
   return newUser;
+}
+
+async function addFriend (id, friend, pending = true) {
+  const friendObject = {
+    userId: id,
+    friendId: friend,
+    note: '',
+    pending,
+    addedAt: Date.now(),
+    favorited: false
+  }
+  const dbFriend = new models.Friend(friendObject);
+  await dbFriend.save();
+  return friend;
+}
+
+async function getPendingFriendRequests (id) {
+  // Looking for pending relationships requests where the other user has put the current user as the friend ID
+  const pending = await models.Friend.find({ friendId: id, pending: true }, { __v: 0, _id: 0 });
+  if (pending.length) {
+    const users = await getMultiple(pending.map(friend => friend.userId));
+    return users;
+  } else {
+    return [];
+  }
+}
+
+async function acceptFriendRequest (id, friend) {
+  // Update the pending request to be completed
+  const operation = await models.Friend.updateOne({ userId: friend, friendId: id }, { pending: false });
+  if (operation.n) {
+    // The accepter now adds the acceptee as a friend, establishing a relationship
+    await addFriend(id, friend, false)
+    // Add to content feed for both users
+    HomeContent.create({ user: friend, type: HomeContentTypes.FRIEND_REQUEST_ACCEPTED, value: { user: id } });
+    HomeContent.create({ user: id, type: HomeContentTypes.FRIENDSHIP_CREATED, value: { user: friend } });
+  }
+  return { accepted: true };
+}
+
+async function editFriend (id, friend, data) {
+  const friendObject = {
+    note: data.note,
+    favorited: data.favorited
+  }
+  await models.Friend.updateOne({ userId: id, friendId: friend }, friendObject);
+  return true;
+}
+
+async function removeFriend (id, friend) {
+  // If one friend removes another, both should be removed
+  await models.Friend.deleteOne({ userId: id, friendId: friend });
+  await models.Friend.deleteOne({ userId: friend, friendId: id });
+  return true;
+}
+
+async function getFriends (id) {
+  return await models.Friend.find({ userId: id, pending: false }, { __v: 0, _id: 0 });
 }
 
 async function listBoards (id) {
@@ -121,6 +183,46 @@ async function logout (id) {
   return true;
 }
 
+async function getHomeContent (id) {
+  /*
+  const groupArrayByTimestamps = (array, range, timestampProperty = 'createdAt') => {
+    const final = [];
+    let current = [];
+    for (const value of array) {
+      if (Date.now() - new Date(value[timestampProperty]).valueOf() < range) {
+        current.push(value);
+      } else {
+        final.push(current);
+        current = [];
+      }
+    }
+    if (current.length) {
+      final.push(current);
+    }
+    return final;
+  }
+  */
+
+  const REMOVE_INSIGNIFICANT = { __v: 0, _id: 0 };
+  // Get notes where this user is the creator or assigned
+  const notes = await models.Note.find({ $or: [{ 'createdBy.id': id }, 
+    { assignees: { $in: id } }] }, REMOVE_INSIGNIFICANT);
+  const content = await models.HomeContent.find({ user: id }, REMOVE_INSIGNIFICANT);
+  console.log(content)
+  if (notes.length) {
+    for (let i = 0; i < notes.length; i++) {
+      const note = notes[i];
+      if (note.dueDate) {
+        console.log('adding due date to home content')
+        content.push({ type: HomeContentTypes.DUE_DATE_UPCOMING, value: { note } })
+      }
+    }
+  }
+
+  return content;
+}
+
+
 export default {
   authenticate,
   create,
@@ -128,5 +230,12 @@ export default {
   get,
   edit,
   getMultiple,
-  logout
+  logout,
+  getHomeContent,
+  getFriends,
+  addFriend,
+  removeFriend,
+  editFriend,
+  acceptFriendRequest,
+  getPendingFriendRequests
 }
